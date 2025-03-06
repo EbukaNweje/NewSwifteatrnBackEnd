a4b189605766b493480c82c5e83a4d93dd3e3baa
EbukaNweje <nwejeebuka@gmail.com>
2025-03-06 15:02:49 -0800

    F

--- controllers/auth.js
+++ controllers/auth.js
@@ -361,9 +361,9 @@ exports.AdminAproveEmailSand = async (req, res, next) =>{
     
             <div class="content">
                 <p>Hi ${UserEmail.userName},</p>
-                <p>Your Trading account has been approved successfully.<br><br>Folle this link to login: https://swifteatrn-prime-dash-board.vercel.app/ <br><br>You can go ahead and fund your Trade account to start up your Trade immediately. Deposit through Bitcoin.</p>
-                <p>For more enquiries, kindly contact your account manager or use our live chat support on our platform. You can also send a direct mail to us at <span style="color: #4c7fff;">${process.env.USER}</span></p>
-                <p>Thank you for choosing our platform. We wish you successful trading.</p>
+                <span>Your Trading account has been approved successfully. <br><br>Folle this link to login: https://swifteatrn-prime-dash-board.vercel.app/ <br><br>You can go ahead and fund your Trade account to start up your Trade immediately.</span>
+                <span>For more enquiries, kindly contact your account manager or use our live chat support on our platform. You can also send a direct mail to us at <span style="color: #4c7fff;">${process.env.USER}</span></span>
+                <span>Thank you for choosing our platform. We wish you successful trading.</span>
             </div>
     
             <div class="footer">
